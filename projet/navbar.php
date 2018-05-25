3a3ce5238be34735114bed708455d4c62bd4b382
Fabien Couthouis <fcouthouis@ensc.fr>
2018-05-25 16:41:55 +0200

    pdf

---
+++ projet/navbar.php
@@ -0,0 +1,35 @@
+<div class="navbar navbar-inverse navbar-fixed-top headroom" >
+		<div class="container">
+			<div class="navbar-header">
+				<!-- Button for smallest screens -->
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+				<a class="navbar-brand" href="../index.html"><img src="../assets/images/logo.png" alt="Progressus HTML5 template"></a>
+			</div>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav pull-right">
+					<li><a href="../index.html">Accueil</a></li>
+					<li><a href="../team.html">La Team</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Projet<b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li><a href="objectifs.html">Objectifs</a></li>
+							<li><a href="materiel.html">Matériel et pré-requis</a></li>
+							<li><a href="donnees.html">Recueil des données</a></li>
+							<li><a href="tuto_utilisation.html">Documentation technique</a></li>
+						
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Soutenance<b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li><a href="../soutenance/introduction.html">Introduction</a></li>
+							<li><a href="../soutenance/evolution.html">Évolutions du projet</a></li>
+							<li><a href="../soutenance/gestion-projet.html">Gestion de projet</a></li>
+							<li><a href="../soutenance/futur.html">Avancements futurs</a></li>
+						</ul>
+					</li>
+					<li><a class="btn" href="https://github.com/Fabien-Couthouis/SmartMirror" target="_blank"> <i class="fa fa-github"></i> Github SmartMirror</a></li>
+				</ul>
+			</div><!--/.nav-collapse -->
+		</div>
+	</div> 
